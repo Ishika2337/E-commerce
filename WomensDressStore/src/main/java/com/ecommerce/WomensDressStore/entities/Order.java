98e7f47f42f77d7bc31752c657dbce4b4250f2fd
Ishika Verma <ishikaverma496g@gmail.com>
2022-02-03 10:33:00 +0530

    Upload photo usiing drive link

---
+++ WomensDressStore/src/main/java/com/ecommerce/WomensDressStore/entities/Order.java
@@ -0,0 +1,51 @@
+package com.ecommerce.WomensDressStore.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+    @Id
+    private Long id;
+    private String brand;
+    private String dressUrl;
+    private Double cost;
+
+    public Order(String brand, String dressUrl, Double cost) {
+        this.brand = brand;
+        this.dressUrl = dressUrl;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDressUrl() {
+        return dressUrl;
+    }
+
+    public void setDressUrl(String dressUrl) {
+        this.dressUrl = dressUrl;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+}
